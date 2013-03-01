394392da7ea7147756770805d8997065ff2e6e72
Jon Conley <conley.jj@gmail.com>
2013-03-01 13:30:19 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Violin2::Application.config.secret_token = 'b80eadba41f7c98edc1cf1757f368805cd3894639f0989c4ffd9f2e14ba6a1474cb8584aa6a956238f8281244eac0348e8ccfaf9d22ddd94c393996090c37040'
